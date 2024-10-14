6960f39564f40dbfe9fbdb1dcb11d9b3f76088a4
Matt Vella <mcvella@gmail.com>
2024-10-14 18:38:13 -0400

    activate venv

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ if ! command -v uv 2>&1 >/dev/null; then
     pip install uv
 fi
 uv venv --python 3.12
+source .venv/bin/activate
 uv pip install -r requirements.txt
 .venv/bin/python3 -m PyInstaller --onefile --paths src --hidden-import="googleapiclient" main.py
 tar -czvf dist/archive.tar.gz dist/main
