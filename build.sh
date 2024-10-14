41373f141ec3637d6d13a4220c254d32bbdc262b
Abe Winter <abe-winter@users.noreply.github.com>
2024-10-14 17:54:30 -0400

    shebangs, chmod +x, build new main.py path

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 sudo apt-get install -y python3.11-venv
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --onefile --paths src --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --onefile --paths src --hidden-import="googleapiclient" main.py
 tar -czvf dist/archive.tar.gz dist/main
