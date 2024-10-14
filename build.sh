17f83130fe0368bc441e18d3b9ccbb651bda899f
Matt Vella <mcvella@gmail.com>
2024-10-14 17:10:41 -0400

    try paths for pyinstaller

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y python3.11-venv
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --onefile --paths src --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
